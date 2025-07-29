69200cd57ccd628cb4b2af160d4795833e79e6af
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-29 15:21:01 +0700

    update readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from '@/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
