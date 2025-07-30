ba6ef7fddb3fde889d7e47b505a4a1edfa67890e
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-31 02:28:01 +0700

    Swagger

--- src/modules/cart/dto/cart.dto.ts
+++ src/modules/cart/dto/cart.dto.ts
@@ -1,28 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsNumber, Min } from 'class-validator';
 
 export class AddToCartDto {
+  @ApiProperty({ example: '918390281938921' })
   @IsNotEmpty()
   @IsString()
   productId: string;
 
+  @ApiProperty({ example: '918390281938921' })
   @IsNotEmpty()
   @IsString()
   userId: string;
 
+  @ApiProperty({ example: 1 })
   @IsNumber()
   @Min(1)
   quantity: number;
 }
 
 export class UpadteCartQuantityDto {
+  @ApiProperty({ example: '918390281938921' })
   @IsNotEmpty()
   @IsString()
   productId: string;
 
+  @ApiProperty({ example: '918390281938921' })
   @IsNotEmpty()
   @IsString()
   userId: string;
 
+  @ApiProperty({ example: 2 })
   @IsNumber()
   quantityChange: number;
 }
--- src/modules/categories/dto/categories.dto.ts
+++ src/modules/categories/dto/categories.dto.ts
@@ -1,6 +1,8 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CategoriesDto {
+  @ApiPropertyOptional({ example: 'Category name' })
   @IsNotEmpty()
   @IsString()
   name: string;
