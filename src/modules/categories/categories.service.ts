0015e072e5f0e5e438d9c4651dc6f70ae2ba237a
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-31 14:04:32 +0700

    Category unit testing

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '@/prisma.service';
 import { CategoriesDto } from '@/modules/categories/dto/categories.dto';
 
 @Injectable()
